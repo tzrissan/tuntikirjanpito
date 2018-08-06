0fa0c6453d12b58886b58fdfa8223f552ec183ab
Tuomo Rissanen <tuomo.rissanen@iki.fi>
2018-08-07 00:48:27 +0300

    Init vue app

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Lomake />
+  </div>
+</template>
+
+<script>
+import Lomake from './components/Lomake.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Lomake
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Lomake.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+  <form>
+    <label for="tuloaika">Tuloaika</label>
+    <input type="text" id="tuloaika" v-model="tuloaika" /><br/>
+    <label for="lahtoaika" v-if="tuloaika">Lähtöaika</label>
+    <input type="text" id="lahtoaika" v-model="lahtoaika" v-if="tuloaika" /><br/>
+    <input type="button" value="Tallenna" v-if="tuloaika && lahtoaika" v-on:click="tallenna"></input>
+  </form>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'Lomake',
+  props: {
+
+  },
+  methods: {
+    tallenna() {
+      console.log("tallenna");
+      console.log({tuloaika: this.tuloaika, lahtoaika: this.lahtoaika});
+      this.tuloaika=undefined;
+      this.lahtoaika=undefined;
+    }
+  },
+  data() {
+    return {
+      tuloaika: undefined,
+      lahtoaika: undefined
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  label {
+    display: inline-block;
+    width: 100px;
+    text-align: left;
+    margin: 5px 0;
+  }
+  input {
+    width: 200px;
+    text-align: left;
+    margin: 5px 0;
+    padding: 2px 15px;
+  }
+</style>
