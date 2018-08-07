5c3080f32f8e7dd2badd4e803d391235732cb38e
Tuomo Rissanen <tuomo.rissanen@solita.fi>
2018-08-07 17:18:11 +0300

    Helppo tapa saada tämä ajanhetki kenttään

--- src/components/Lomake.vue
+++ src/components/Lomake.vue
@@ -1,19 +1,28 @@
 <template>
     <div class="hello">
         <h1>{{ today | moment("D.M.YYYY") }}</h1>
-        <form>
-            <label for="tuloaika">Tuloaika</label>
-            <input type="text" id="tuloaika" v-model="tuloaika"/><br/>
+        <form @submit="tallenna">
+            <label for="date">Päivä</label>
+            <input type="text" id="date" v-model="date"/><br/>
+
+            <label for="tuloaika" >Tuloaika</label>
+            <input type="text" id="tuloaika" v-model="tuloaika"/>
+            <input type="button" class="now" value="< now" v-on:click="tulinJust()" /><br/>
+
             <label for="lahtoaika" v-if="tuloaika">Lähtöaika</label>
-            <input type="text" id="lahtoaika" v-model="lahtoaika" v-if="tuloaika"/><br/>
-            <input type="button" value="Tallenna" v-if="tuloaika || lahtoaika" v-on:click="tallenna" />
+            <input type="text" id="lahtoaika" v-model="lahtoaika" v-if="tuloaika"/>
+            <input type="button" class="now" value="< now"  v-if="tuloaika" v-on:click="meenIhanKohta()" /><br/>
+
+            <input type="submit" value="Tallenna" v-if="tuloaika && lahtoaika" />
         </form>
         <table>
             <tbody>
-            <tr v-for="tyoaika in tyoajat" v-bind:key="tyoaika.id">
+            <tr v-for="tyoaika in laskevassaJarjestyksessa(tyoajat)" v-bind:key="tyoaika.id">
                 <td>{{ tyoaika.id }}</td>
+                <td>{{ tyoaika.date | moment("D.M.YYYY") }}</td>
                 <td>{{ tyoaika.tuloaika }}</td>
                 <td>{{ tyoaika.lahtoaika }}</td>
+                <td>{{ tyoaika.lounaat }}</td>
             </tr>
             </tbody>
         </table>
@@ -22,25 +31,50 @@
 
 <script>
 
+    import _ from 'lodash';
+    import numeral from 'numeral';
     import Tuntikirjanpito from '../data.js';
+
+    function nyt() {
+        const now = new Date();
+        const hour = parseInt(now.toISOString().replace(/.*T(\d\d):.*/, '$1'));
+        const minute = parseInt(now.toISOString().replace(/.*T\d\d:(\d\d).*/, '$1'));
+        const hourFormatted = numeral(hour).format('00');
+        const minuteRoundedAndFormatted = numeral(Math.round(minute/5)*5).format('00');
+        return `${hourFormatted}:${minuteRoundedAndFormatted}`;
+    }
 
     export default {
         name: 'Lomake',
         props: {},
         methods: {
-            tallenna() {
+            tallenna(e) {
                 this.tyoajat.push({
                     id: Math.floor(Math.random() * 1000000),
+                    date: this.date.replace(/(\d{1,2})\.(\d{1,2})\.(\d{4})/, '$3-$2-$1'),
                     tuloaika: this.tuloaika,
-                    lahtoaika: this.lahtoaika
+                    lahtoaika: this.lahtoaika,
+                    lounaat: 1
                 });
+                this.date = new Date().toISOString().replace(/(\d{4})-(\d{2})-(\d{2}).*/, '$3.$2.$1');
                 this.tuloaika = undefined;
                 this.lahtoaika = undefined;
-            }
+                e.preventDefault();
+            },
+            laskevassaJarjestyksessa(rivit) {
+                return _.sortBy(rivit, 'date').reverse();
+            },
+            tulinJust() {
+                this.tuloaika = nyt()
+            },
+            meenIhanKohta() {
+                this.lahtoaika = nyt()
+            },
         },
         data() {
             return {
                 today: new Date(),
+                date: new Date().toISOString().replace(/(\d{4})-(\d{2})-(\d{2}).*/, '$3.$2.$1'),
                 tuloaika: undefined,
                 lahtoaika: undefined,
                 tyoajat: Tuntikirjanpito.get()
@@ -59,9 +93,14 @@
     }
 
     input {
-        width: 200px;
+        width: 150px;
         text-align: left;
         margin: 5px 0;
         padding: 2px 15px;
+    }
+
+    input.now {
+        width: initial;
+        padding: 2px 5px;
     }
 </style>
--- src/data.js
+++ src/data.js
@@ -1,22 +1,22 @@
 // IDEA replace
 // (\d{1,2}\.\d{1,2}\.)\s*[a-zA-Z\s]*(\d{1,2}:\d\d)[\s-]*(\d{1,2}:\d\d).*(\d)
-// { date: '$1', tuloaika: '$2', lahtoaika: '$3', lounaat: $4 },
+// { date: '$1.2018', tuloaika: '$2', lahtoaika: '$3', lounaat: $4 },
 
 const demo_data = [
-    {id: 1, date: '2.1.', tuloaika: '9:05', lahtoaika: '16:50', lounaat: 1},
-    {id: 2, date: '3.1.', tuloaika: '9:10', lahtoaika: '17:30', lounaat: 2},
-    {id: 3, date: '4.1.', tuloaika: '9:05', lahtoaika: '17:00', lounaat: 1},
-    {id: 4, date: '5.1.', tuloaika: '8:45', lahtoaika: '16:20', lounaat: 1},
-    {id: 5, date: '9.1.', tuloaika: '8:50', lahtoaika: '17:25', lounaat: 2},
-    {id: 6, date: '10.1.', tuloaika: '9:10', lahtoaika: '18:00', lounaat: 1},
-    {id: 7, date: '11.1.', tuloaika: '8:55', lahtoaika: '17:15', lounaat: 1},
-    {id: 8, date: '12.1.', tuloaika: '9:05', lahtoaika: '16:00', lounaat: 1}
+    {id: 1, date: '2018-01-02', tuloaika: '9:05', lahtoaika: '16:50', lounaat: 1},
+    {id: 2, date: '2018-01-03', tuloaika: '9:10', lahtoaika: '17:30', lounaat: 2},
+    {id: 3, date: '2018-01-04', tuloaika: '9:05', lahtoaika: '17:00', lounaat: 1},
+    {id: 4, date: '2018-01-05', tuloaika: '8:45', lahtoaika: '16:20', lounaat: 1},
+    {id: 5, date: '2018-01-09', tuloaika: '8:50', lahtoaika: '17:25', lounaat: 2},
+    {id: 6, date: '2018-01-10', tuloaika: '9:10', lahtoaika: '18:00', lounaat: 1},
+    {id: 7, date: '2018-01-11', tuloaika: '8:55', lahtoaika: '17:15', lounaat: 1},
+    {id: 8, date: '2018-01-12', tuloaika: '9:05', lahtoaika: '16:00', lounaat: 1}
 ];
 
 const Tuntikirjanpito = {
     get() {
         return demo_data;
     }
-}
+};
 
 export default Tuntikirjanpito;
