6215312ec0405739a6d5128f3b75e975dd297383
Tuomo Rissanen <tuomo.rissanen@solita.fi>
2018-08-07 16:20:40 +0300

    Näytetään data jotenkin

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <div id="app">
-    <Lomake />
-  </div>
+    <div id="app">
+        <Lomake/>
+    </div>
 </template>
 
 <script>
-import Lomake from './components/Lomake.vue'
+    import Lomake from './components/Lomake.vue'
 
-export default {
-  name: 'app',
-  components: {
-    Lomake
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            Lomake
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/Lomake.vue
+++ src/components/Lomake.vue
@@ -1,53 +1,67 @@
 <template>
-  <div class="hello">
-  <form>
-    <label for="tuloaika">Tuloaika</label>
-    <input type="text" id="tuloaika" v-model="tuloaika" /><br/>
-    <label for="lahtoaika" v-if="tuloaika">Lähtöaika</label>
-    <input type="text" id="lahtoaika" v-model="lahtoaika" v-if="tuloaika" /><br/>
-    <input type="button" value="Tallenna" v-if="tuloaika && lahtoaika" v-on:click="tallenna"></input>
-  </form>
-  </div>
+    <div class="hello">
+        <h1>{{ today | moment("D.M.YYYY") }}</h1>
+        <form>
+            <label for="tuloaika">Tuloaika</label>
+            <input type="text" id="tuloaika" v-model="tuloaika"/><br/>
+            <label for="lahtoaika" v-if="tuloaika">Lähtöaika</label>
+            <input type="text" id="lahtoaika" v-model="lahtoaika" v-if="tuloaika"/><br/>
+            <input type="button" value="Tallenna" v-if="tuloaika || lahtoaika" v-on:click="tallenna" />
+        </form>
+        <table>
+            <tbody>
+            <tr v-for="tyoaika in tyoajat" v-bind:key="tyoaika.id">
+                <td>{{ tyoaika.id }}</td>
+                <td>{{ tyoaika.tuloaika }}</td>
+                <td>{{ tyoaika.lahtoaika }}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
 
+    import Tuntikirjanpito from '../data.js';
 
-
-export default {
-  name: 'Lomake',
-  props: {
-
-  },
-  methods: {
-    tallenna() {
-      console.log("tallenna");
-      console.log({tuloaika: this.tuloaika, lahtoaika: this.lahtoaika});
-      this.tuloaika=undefined;
-      this.lahtoaika=undefined;
+    export default {
+        name: 'Lomake',
+        props: {},
+        methods: {
+            tallenna() {
+                this.tyoajat.push({
+                    id: Math.floor(Math.random() * 1000000),
+                    tuloaika: this.tuloaika,
+                    lahtoaika: this.lahtoaika
+                });
+                this.tuloaika = undefined;
+                this.lahtoaika = undefined;
+            }
+        },
+        data() {
+            return {
+                today: new Date(),
+                tuloaika: undefined,
+                lahtoaika: undefined,
+                tyoajat: Tuntikirjanpito.get()
+            }
+        }
     }
-  },
-  data() {
-    return {
-      tuloaika: undefined,
-      lahtoaika: undefined
-    }
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  label {
-    display: inline-block;
-    width: 100px;
-    text-align: left;
-    margin: 5px 0;
-  }
-  input {
-    width: 200px;
-    text-align: left;
-    margin: 5px 0;
-    padding: 2px 15px;
-  }
+    label {
+        display: inline-block;
+        width: 100px;
+        text-align: left;
+        margin: 5px 0;
+    }
+
+    input {
+        width: 200px;
+        text-align: left;
+        margin: 5px 0;
+        padding: 2px 15px;
+    }
 </style>
