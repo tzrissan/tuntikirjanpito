b7d53562b1554345f4cd3de0e3046909eb21bc72
Tuomo Rissanen <tuomo.rissanen@solita.fi>
2018-08-15 13:22:36 +0300

    Konversioskripti parempaan kuosiin

--- convert.py
+++ convert.py
@@ -5,14 +5,14 @@ import re
 excelExport = open('data-2018.txt', 'r');
 output = open('data-2018.sql', 'w');
 
-output.write('INSERT INTO tunnit(id, date, tuloaika, lahtoaika, lounaita, kirjaus) VALUES \n')
+output.write('INSERT INTO tunnit(date, tuloaika, lahtoaika, lounaita, kirjaus, kommentti) VALUES \n')
 
 first = True;
 
 for line in excelExport:
     tokens = line.split('\t');
-    if len(tokens) == 30:
-        #tyopaiva = tokens[3] == 'T'
+    if len(tokens) > 42:
+        tyopaiva = tokens[3] == 'T'
         #lomapaiva = tokens[4] <> ''
         #saikku = tokens[5] <> ''
         #pyha = tokens[6] <> ''
@@ -21,25 +21,32 @@ for line in excelExport:
 
         #print tokens[0] + '-> ' + str(tyopaiva) + ' ' + str(lomapaiva) + ' ' + str(saikku) + ' ' + str(pyha) + ' ' + str(muu) + ' ' + str(ylityo)
 
-        def toDate(value):
-            tokens = value.split(".")
-            return "2018-" + tokens[1].zfill(2) + "-" + tokens[0].zfill(2)
+        kommentti = 'null'
+        if len(tokens) == 44:
+            kommentti = "'" + tokens[43].strip() + "'"
 
-        def toTime(value):
-            tokens = value.split(":")
-            return tokens[0].zfill(2) + ":" + tokens[1].zfill(2)
+        if tyopaiva:
+            def toDate(value):
+                tokens = value.split(".")
+                return "'2018-" + tokens[1].zfill(2) + "-" + tokens[0].zfill(2) + "'"
 
-        if tokens[13] <> '':
-            if not first:
-                output.write(", \n")
+            def toTime(value):
+                tokens = value.split(":")
+                return "'" + tokens[0].zfill(2) + ":" + tokens[1].zfill(2) + "'"
+
+            if tokens[13] <> '':
+                if not first:
+                    output.write(", \n")
+                else:
+                    first = False;
+
+                output.write("( " + toDate(tokens[0]) + ", " + toTime(tokens[11]) + ", " + toTime(tokens[13]) + ", null, null, " + kommentti + " ), \n" )
+                output.write("( " + toDate(tokens[0]) + ", " + toTime(tokens[15]) + ", " + toTime(tokens[17]) + ", " + tokens[19] + ", " + tokens[29].split()[0] + ", " + kommentti + " )" )
             else:
-                first = False;
-
-            output.write("( '" + toDate(tokens[0]) + "', '" + toTime(tokens[11]) + "', '" + toTime(tokens[13]) + "', null, null ), \n" )
-            output.write("( '" + toDate(tokens[0]) + "', '" + toTime(tokens[15]) + "', '" + toTime(tokens[17]) + "', " + tokens[19] + ", " + tokens[29].split()[0] + " )" )
-        else:
-            if not first:
-                output.write(", \n")
-            else:
-                first = False;
-            output.write("( '" + toDate(tokens[0]) + "', '" + toTime(tokens[11]) + "', '" + toTime(tokens[17]) + "', " + tokens[19] + ", " + tokens[29].split()[0] + " )" )
+                if not first:
+                    output.write(", \n")
+                else:
+                    first = False;
+                output.write("( " + toDate(tokens[0]) + ", " + toTime(tokens[11]) + ", " + toTime(tokens[17]) + ", " + tokens[19] + ", " + tokens[29].split()[0] + ", " + kommentti + " )" )
+    else:
+        print tokens[0] + ' ' + str(len(tokens))
